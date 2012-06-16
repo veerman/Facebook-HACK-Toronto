7218ba5d49ddd7ec430ef7e4248d1dae5b843692
Steve Veerman <steve@veerman.ca>
2012-06-16 15:31:19 -0400

    RSS script updates

--- RSS.php
+++ RSS.php
@@ -1,12 +1,14 @@
 <?php
 
-require 'php-sdk/src/facebook.php';
 include realpath($_SERVER['DOCUMENT_ROOT']).'/db.php';
 
-//header('P3P: CP="CAO PSA OUR"');
-//header('Content-type: application/json');
+header('P3P: CP="CAO PSA OUR"');
+//header('Content-type: application/rss+xml');
 
 $uid = !empty($_GET['uid']) ? mysql_real_escape_string($_GET['uid']) : '0';
+
+$row = mysql_fetch_assoc(getRSS($uid));
+echo stripslashes($row['rss']);
 
 function getRSS($uid){
 	$sql = "SELECT `uid`,`rss` FROM `slideshow` WHERE `uid`='$uid'";
